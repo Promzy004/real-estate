d60c5fd72193ad6a895cd0d5498acd98c44cd04b
Promzy004 <promiseedwin242@gmail.com>
2025-05-02 00:16:24 +0100

    header icons added

--- src/compoents/header.jsx
+++ src/compoents/header.jsx
@@ -1,24 +1,39 @@
 import { Link } from "react-router-dom";
+import { FaUser, FaPhone, FaTelegram } from "react-icons/fa6";
+import { FaTelegramPlane } from "react-icons/fa";
+import { FiMenu } from "react-icons/fi";
 
 const Header = () => {
     return (
         <header className="static top-0 w-full">
-            <div className="flex justify-between py-1 px-48 bg-blue-400">
-                <div>
-                    <a href="mailto:">mail.uremail.com</a>
-                    <a href="tel:+">mail.uremail.com</a>
+            <div className="flex justify-between py-2 px-48 bg-blue-400 text-white">
+                <div className="flex justify-center items-center gap-5">
+                    <a href="mailto:" className="flex justify-center items-center gap-2">
+                        <FaTelegramPlane className="text-2xl" />
+                        <span className="font-light">mail.uremail.com</span>
+                    </a>
+                    <a href="tel:+" className="flex justify-center items-center gap-2">
+                        <FaPhone className="text-xl" />
+                        <span className="font-light">mail.uremail.com</span>
+                    </a>
                 </div>
-                <a href="">Login/Signup</a>
+                <a href="" className="flex justify-center items-center gap-2">
+                    <FaUser className="text-xl" />
+                    <span className="font-light">Login/Signup</span>
+                </a>
             </div>
             <div className="flex justify-between py-4 px-48">
                 <h2>Brand name</h2>
-                <nav className="flex gap-7">
+                <nav className="flex gap-7 items-center justify-center">
                     <Link to="" className="hover:text-blue-400">Home</Link>
                     <Link to="" className="hover:text-blue-400">About Us</Link>
                     <Link to="" className="hover:text-blue-400">Property</Link>
                     <Link to="" className="hover:text-blue-400">Agents</Link>
                     <Link to="" className="hover:text-blue-400">Pages</Link>
                     <Link to="" className="hover:text-blue-400">Contact Us</Link>
+                    <div className="bg-blue-400 p-2">
+                        <FiMenu className="text-xl text-white"/>
+                    </div>
                 </nav>
             </div>
         </header>
--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -9,6 +9,10 @@ const Home = () => {
                     <div></div>
                     <div>
                         <h3>Are You Looking Best Property Near You? Contact Us</h3>
+                        <div>
+                            <h4>Who We Are</h4>
+                            <p>It is a long established fact that a reader will be distracted by the readable content of a page When looking at its layout. The point of using Lorem Ipsum is that it has normal distribution. <br /> It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout.</p>
+                        </div>
                     </div>
                 </div>
                 <div></div>
