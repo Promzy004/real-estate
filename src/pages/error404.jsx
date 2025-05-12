c48cf6362a008a81f4303e07f1d7858f2a111083
Promzy004 <promiseedwin242@gmail.com>
2025-05-12 12:56:07 +0100

    error404 adjusted for mobile devices

--- src/pages/error404.jsx
+++ src/pages/error404.jsx
@@ -11,14 +11,14 @@ const Error404 = () => {
 
     return (
         <div className="mt-[120px] md:py-20 py-14 bg-primaryColor/10">
-            <div className="flex flex-col gap-5 justify-center items-center w-[30%] mx-auto">
+            <div className="flex flex-col gap-7 justify-center items-center sm:w-[22rem] w-[14rem] mx-auto">
                 <div className="relative w-full flex justify-center items-end">
-                    <h3 className="text-[10rem] leading-none font-semibold">404</h3>
-                    <div className="bg-primaryColor w-full h-16 absolute left-0 bottom-0 -z-10"></div>
+                    <h3 className="sm:text-[10rem] sm:leading-none text-8xl font-semibold">404</h3>
+                    <div className="bg-primaryColor w-full sm:h-16 h-10 absolute left-0 bottom-0 -z-10"></div>
                 </div>
                 <div className="flex flex-col justify-center gap-2 items-center">
-                    <h4 className="text-xl font-medium">Sorry Page Not Found</h4>
-                    <p className="text-center text-sm font-light">
+                    <h4 className="sm:text-xl tex-lg font-medium">Sorry Page Not Found</h4>
+                    <p className="text-center sm:text-sm text-xs font-light">
                         We are sorry. The page you are looking for does not exist. <br />
                         It might have been moved or deleted.
                     </p>
